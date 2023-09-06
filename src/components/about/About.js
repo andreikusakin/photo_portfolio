6f9fd016bb730226bd9cba48dc70d6ed544d660b
andrewkusakin <akusaki@wgu.edu>
2023-09-05 22:46:09 -0400

    added image and text

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,24 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
 
 export default function About() {
   return (
-    <div><p>Hi! My name is Andrew</p></div>
-  )
+    <div className="about-page">
+      <div className="about-bg"></div>
+      <div className="about-section">
+        <img src="/images/aboutphoto.jpg" alt="photo"/>
+        <div className="about-text"><p> Hello! I'm Andrew Kusakin. </p>
+      <p>
+       Art has held a profound place in my heart for
+        as long as I can remember. It's not just something I do, but a way for
+        me to express myself. I've always enjoyed capturing moments—whether I'm
+        drawing them or snapping photos with my camera. For me, every picture or
+        drawing is a way to freeze a special moment in time, and I love being
+        able to share those with others. But my creativity doesn't stop at
+        visual arts; it also stretches into the world of music and technology.{" "}
+      </p></div></div>
+      
+      
+    </div>
+  );
 }
