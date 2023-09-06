154807da914048516529fafdf4b4de04c68b8925
andrewkusakin <akusaki@wgu.edu>
2023-09-05 23:34:26 -0400

    added object into array

--- src/components/travel/Travel.js
+++ src/components/travel/Travel.js
@@ -1,30 +1,28 @@
-import React, { useState } from "react";
-import Gallery from "../gallery/Gallery";
-import { imagesTravel } from "../../images"
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import "./travel.css";
+import Section from "../section/Section";
+import Loader from "../loader/Loader";
 
-const travelList = [{
-  names: "California",
-  gallery: imagesTravel,
-  coverImg: ""
-},
-{
-  names: "Nevada",
-  gallery: imagesTravel,
-  coverImg: ""
-},
-]
+const travelList = [
+  {
+    name: "California 2021",
+    pathName: "california-2021",
+    coverImg: "/images/california2021.jpg",
+  },
+  {
+    name: "Vermont 2022",
+    pathName: "vermont-2022",
+    coverImg: "/images/vermont2022.jpg",
+  },
+];
 
-export default function Travel() {
-
-  const [imageArray, setImageArray] = useState([]); 
-
+export default function Wedding() {
   return (
     <div>
-      {travelList.map((i) => (<Link to={`place/${i.names}` }><div onClick={() => setImageArray(i.gallery)}>
-        <p>{i.names}</p>
-      </div></Link>))}
-      <Outlet context={[imageArray, setImageArray]} />
+      <Loader />
+
+      <Outlet />
+      <Section list={travelList} />
     </div>
   );
 }
