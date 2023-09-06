5763b62dfd0b144950a6e0bd606e8fd73b8714cc
andrewkusakin <akusaki@wgu.edu>
2023-09-05 23:32:01 -0400

    updated styling

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,7 +6,7 @@ export default function About() {
     <div className="about-page">
       <div className="about-bg"></div>
       <div className="about-section">
-        <img src="/images/aboutphoto.jpg" alt="photo"/>
+        
         <div className="about-text"><p> Hello! I'm Andrew Kusakin. </p>
       <p>
        Art has held a profound place in my heart for
@@ -15,8 +15,11 @@ export default function About() {
         drawing them or snapping photos with my camera. For me, every picture or
         drawing is a way to freeze a special moment in time, and I love being
         able to share those with others. But my creativity doesn't stop at
-        visual arts; it also stretches into the world of music and technology.{" "}
-      </p></div></div>
+        visual arts; it also stretches into the world of music and technology.
+      </p></div>
+      
+      <div className="about-photo"></div>
+      </div>
       
       
     </div>
